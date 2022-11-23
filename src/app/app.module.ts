1685ecdb427c495b44fae17f00c46727ae06f4ac
TheFlorin97 <constantin.florin97@yahoo.com>
2022-11-23 22:16:57 +0200

    m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
+// ANGULAR CORE
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+// ANGULAR MATERIAL
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { TopNavComponent } from './main/top-nav/top-nav.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+// THIRD PARTY
 
 @NgModule({
   declarations: [
@@ -16,7 +29,13 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-nav.component.css']
 })
 export class TopNavComponent {
-
+  value = 'Clear me';
 }
